3002399389eeda4cd8479f14b729424749b1bc14
yui880 <kimyuna521@gmail.com>
2024-04-01 23:56:38 +0900

    [4/1] 백준 알고리즘 1문제

---
+++ Beakjoon/Gold/감소하는_수.js
@@ -0,0 +1,27 @@
+//1038
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "Beakjoon/Gold/test.txt";
+let input = fs.readFileSync(filePath).toString().trim();
+
+const N = Number(input);
+let list = new Set([]);
+
+const backTracking = (num, count) => {
+    if (count === 10) {
+        if (!list.has(num)) list.add(num);
+        return;
+    }
+
+    backTracking(num, count + 1);
+    backTracking(num * 10 + 9 - count, count + 1);
+};
+
+backTracking(0, 0);
+
+const sorted = [...list].sort((a, b) => a - b);
+
+if (N < list.size) {
+    console.log(sorted[N]);
+} else {
+    console.log(-1);
+}
