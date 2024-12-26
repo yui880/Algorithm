4f3f8bc2ffb9374c3773603bbce588c243cc689d
김유나 <58588976+yui880@users.noreply.github.com>
2024-12-26 11:09:49 +0900

    [level 3] Title: 모두 0으로 만들기, Time: 1039.98 ms, Memory: 245 MB -BaekjoonHub

---
+++ 프로그래머스/3/76503. 모두 0으로 만들기/모두 0으로 만들기.js
@@ -0,0 +1,46 @@
+function solution(a, edges) {
+    let sum = a.reduce((sum, cur)=> sum+cur, 0)
+    if(sum !== 0) return -1;
+    
+    const tree = {};
+    
+    for(const [u,v] of edges){
+        tree[u] ? tree[u].push(v) : tree[u] = [v]
+        tree[v] ? tree[v].push(u) : tree[v] = [u]
+    }
+    
+    let leaf = [];
+    
+    for(const key in tree){
+        if(tree[key].length === 1) {
+            leaf.push(key);
+        }
+    }
+
+    const visited = new Set();
+    let answer = 0n;
+    
+    const stack = [[leaf[0], null]]; // [node, parent]
+    const sums = Array(a.length).fill(0n);
+   
+    while(stack.length) {
+       const [node, parent] = stack[stack.length - 1];
+       
+       if(!visited.has(node)) { // 방문한 적 없으면
+           visited.add(node);
+           sums[node] = BigInt(a[node]);
+           
+           for(const next of tree[node]) { // 방문하고 부모 아닌 다음 위치로 이동
+               if(next !== parent) stack.push([next, node]);
+           }
+       } else {
+           stack.pop(); // 방문 했으면 탐색 X
+           if(parent !== null) { // 리프노드 / 루트노드면 해당 위치에 남은 숫자들 정답에 더해주기 
+               sums[parent] += sums[node];
+               answer += BigInt(Math.abs(Number(sums[node])));
+           }
+       }
+   }
+    
+   return answer;
+}
