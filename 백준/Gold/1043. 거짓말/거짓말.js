1d05765fcb46dbe9374bd111c93cd4fbe9189527
김유나 <58588976+yui880@users.noreply.github.com>
2025-01-03 10:56:28 +0900

    [Gold IV] Title: 거짓말, Time: 100 ms, Memory: 9396 KB -BaekjoonHub

--- 백준/Gold/1043. 거짓말/거짓말.js
+++ 백준/Gold/1043. 거짓말/거짓말.js
@@ -6,28 +6,46 @@ let idx = 0;
 const [n, m] = input[idx++].split(" ").map(Number);
 const [tCount, ...truthList] = input[idx++].split(" ").map(Number);
 
-let checkList = new Set(truthList);
-
 const party = [];
 while (input.length > idx) {
   const [_, ...temp] = input[idx++].split(" ").map(Number);
   party.push(temp);
 }
 
+const parents = Array.from({ length: n + 1 }, (_, i) => i);
+
+const find = (x) => {
+  if (x === parents[x]) return x;
+
+  parents[x] = find(parents[x]);
+  return parents[x];
+};
+
+const union = (x, y) => {
+  const rootX = find(x);
+  const rootY = find(y);
+
+  if (rootX === rootY) return;
+
+  if (rootX < rootY) parents[rootY] = rootX;
+  else parents[rootX] = rootY;
+};
+
+// 진실 아는 사람 그룹화하기
+truthList.forEach((val) => union(0, val));
+
+// 파티 사람들 그룹화하기
+for (const p of party) {
+  for (let i = 1; i < p.length; i++) {
+    union(p[0], p[i]);
+  }
+}
+
 let answer = 0;
 
-for (let i = 0; i < m; i++) {
-  for (const list of party) {
-    const test = new Set([...list, ...checkList]);
-
-    if (test.size !== checkList.size + list.length) {
-      checkList = test;
-    } else {
-      if (i === m - 1) {
-        answer++;
-      }
-    }
-  }
+// 파티에 진실그룹 사람 있는지 확인하기
+for (const p of party) {
+  if (find(p[0]) !== 0) answer++;
 }
 
 console.log(answer);
