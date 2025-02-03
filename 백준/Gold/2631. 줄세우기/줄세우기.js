9f838d0fa0f45c9aa9a168ff432e48c4e37c3abe
김유나 <58588976+yui880@users.noreply.github.com>
2025-02-03 15:36:06 +0900

    [Gold IV] Title: 줄세우기, Time: 92 ms, Memory: 9336 KB -BaekjoonHub

--- 백준/Gold/2631. 줄세우기/줄세우기.js
+++ 백준/Gold/2631. 줄세우기/줄세우기.js
@@ -5,13 +5,35 @@ let input = fs.readFileSync(filePath).toString().trim().split("\n");
 const n = Number(input.shift());
 const list = input.map(Number);
 
-const dp = Array(n).fill(1);
+const dp = Array(n);
 
-for (let i = 0; i < n; i++) {
-  for (let j = 0; j < i; j++) {
-    if (list[j] > list[i]) continue;
-    dp[i] = Math.max(dp[j] + 1, dp[i]);
+const binarySearch = (left, right, target) => {
+  while (left < right) {
+    const mid = Math.floor((left + right) / 2);
+
+    if (dp[mid] > target) {
+      right = mid;
+    } else {
+      left = mid + 1;
+    }
+  }
+
+  return left;
+};
+
+dp[0] = list[0];
+let j = 0; // dp의 길이
+
+for (let i = 1; i < n; i++) {
+  if (list[i] > dp[j]) {
+    dp[j + 1] = list[i];
+    j += 1;
+  } else {
+    let idx = binarySearch(0, j, list[i]); // 현재 값 dp 어디에 넣어야 하는지
+    dp[idx] = list[i];
   }
 }
 
-console.log(n - Math.max(...dp));
+const LIS = j + 1;
+
+console.log(n - LIS);
