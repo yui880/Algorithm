4ede35696b60c9937cc900c25c6744438535463c
김유나 <58588976+yui880@users.noreply.github.com>
2025-02-06 09:53:15 +0900

    [Gold IV] Title: 알고스팟, Time: 496 ms, Memory: 93860 KB -BaekjoonHub

---
+++ 백준/Gold/1261. 알고스팟/알고스팟.js
@@ -0,0 +1,40 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "Beakjoon/Gold/test.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+const [m, n] = input.shift().split(" ").map(Number);
+const board = input.map((str) => str.split("").map(Number));
+const wall = Array.from({ length: n }, () => Array(m).fill(Infinity));
+
+const dx = [1, -1, 0, 0];
+const dy = [0, 0, 1, -1];
+
+const bfs = (a, b) => {
+  const queue = [[a, b, 0]];
+  wall[a][b] = 0;
+
+  let idx = 0;
+  while (queue.length > idx) {
+    const [x, y, broken] = queue[idx++];
+
+    for (let i = 0; i < 4; i++) {
+      const nx = x + dx[i];
+      const ny = y + dy[i];
+
+      if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+
+      if (board[nx][ny] === 1 && broken + 1 < wall[nx][ny]) {
+        wall[nx][ny] = broken + 1;
+        queue.push([nx, ny, broken + 1]);
+      }
+      if (board[nx][ny] === 0 && broken < wall[nx][ny]) {
+        wall[nx][ny] = broken;
+        queue.push([nx, ny, broken]);
+      }
+    }
+  }
+
+  return wall[n - 1][m - 1];
+};
+
+console.log(bfs(0, 0));
