5ebd9fdb5f01e68ef6d961195b217012be3086a1
김유나 <58588976+yui880@users.noreply.github.com>
2025-03-18 14:22:01 +0900

    [Gold IV] Title: 비슷한 단어, Time: 392 ms, Memory: 71624 KB -BaekjoonHub

--- 백준/Gold/2179. 비슷한 단어/비슷한 단어.js
+++ 백준/Gold/2179. 비슷한 단어/비슷한 단어.js
@@ -5,32 +5,68 @@ let input = fs.readFileSync(filePath).toString().trim().split("\n");
 const n = +input.shift();
 const list = input;
 
-const checkPrefixLen = (str1, str2) => {
-  let len = 0;
-
-  for (let i = 0; i < str1.length; i++) {
-    if (str1[i] !== str2[i]) {
-      break;
-    } else {
-      len = i + 1;
-    }
-  }
-
-  return len;
-};
-
-let max = 0;
-let answer = [];
-
-for (let i = 0; i < n; i++) {
-  for (let j = i + 1; j < n; j++) {
-    const temp = checkPrefixLen(list[i], list[j]);
-
-    if (temp > max) {
-      max = temp;
-      answer = [list[i], list[j]];
-    }
+class Node {
+  constructor(value = "") {
+    this.value = value;
+    this.children = new Map();
+    this.wordIndices = [];
   }
 }
 
-console.log(answer.join("\n"));
+class Trie {
+  constructor() {
+    this.root = new Node();
+  }
+
+  insert(text, index) {
+    let cur = this.root;
+
+    for (const char of text) {
+      if (!cur.children.has(char)) {
+        cur.children.set(char, new Node(cur.value + char));
+      }
+
+      cur = cur.children.get(char);
+      cur.wordIndices.push(index);
+    }
+  }
+
+  findLongestPrefixPair() {
+    let maxPrefixLength = 0;
+    let pair = [0, 1];
+
+    const dfs = (node, depth) => {
+      if (node.wordIndices.length >= 2) {
+        for (let i = 0; i < node.wordIndices.length; i++) {
+          for (let j = i + 1; j < node.wordIndices.length; j++) {
+            const idx1 = node.wordIndices[i];
+            const idx2 = node.wordIndices[j];
+
+            if (
+              depth > maxPrefixLength ||
+              (depth === maxPrefixLength && (idx1 < pair[0] || (idx1 === pair[0] && idx2 < pair[1])))
+            ) {
+              maxPrefixLength = depth;
+              pair = [idx1, idx2];
+            }
+          }
+        }
+      }
+
+      for (const [char, childNode] of node.children) {
+        dfs(childNode, depth + 1);
+      }
+    };
+
+    dfs(this.root, 0);
+    return pair;
+  }
+}
+
+const trie = new Trie();
+list.forEach((word, index) => {
+  trie.insert(word, index);
+});
+
+const resultPair = trie.findLongestPrefixPair();
+console.log(resultPair.map((v) => list[v]).join("\n"));
