d208c803f280e7dede6d5d58e932de3d506f38f1
김유나 <58588976+yui880@users.noreply.github.com>
2025-03-19 14:35:44 +0900

    [Gold III] Title: 가희와 탑, Time: 188 ms, Memory: 24176 KB -BaekjoonHub

--- 백준/Gold/24337. 가희와 탑/가희와 탑.js
+++ 백준/Gold/24337. 가희와 탑/가희와 탑.js
@@ -4,50 +4,26 @@ let input = fs.readFileSync(filePath).toString().trim().split("\n");
 
 const [n, a, b] = input[0].split(" ").map(Number);
 
-const arr = Array(n).fill(1);
-
 if (a + b > n + 1) {
   console.log(-1);
   process.exit(0);
 }
 
-if (a <= b) {
-  if (a === 1) {
-    arr[0] = b;
-
-    let min = 1;
-    for (let i = n - 1; i > n - b; i--) {
-      arr[i] = min;
-      min++;
-    }
-  } else {
-    let aCount = a - 2;
-    let max = Math.max(a - 1, 1);
-
-    for (let i = n - b - 1; i >= n - b - aCount; i--) {
-      arr[i] = max;
-      if (max > 1) max--;
-    }
-
-    let min = 1;
-    for (let i = n - 1; i >= n - b; i--) {
-      arr[i] = min;
-      min++;
-    }
-  }
-} else {
-  let aCount = a;
-  let max = a;
-
-  for (let i = n - b; i >= b - aCount; i--) {
-    arr[i] = max;
-    if (max > 1) max--;
-  }
-
-  let min = 1;
-  for (let i = n - 1; i > n - b; i--) {
-    arr[i] = min;
-    min++;
-  }
+const answer = [];
+for (let i = 1; i < a; i++) {
+  answer.push(i);
 }
-console.log(arr.join(" "));
+
+answer.push(Math.max(a, b));
+
+for (let i = b - 1; i > 0; i--) {
+  answer.push(i);
+}
+
+if (answer.length > n) {
+  console.log(-1); // 조건을 충족시킬 수 없음
+} else {
+  const oneLen = n - answer.length;
+
+  console.log([answer[0], ...Array(oneLen).fill(1), ...answer.slice(1)].join(" "));
+}
